6d4e3fbd1cc4b2493482a78c2acda4fcb8d3f388
caijuemou <89962192+caijuemou@users.noreply.github.com>
2026-02-10 20:55:18 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
   plugins: [
-    vue(),
-    legacy({
-      targets: ['defaults', 'not IE 11'], // 覆盖大多数现代和主流旧版浏览器
-    }),
+    vue()
   ],
 })
