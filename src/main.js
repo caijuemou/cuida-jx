1973bf78ead5d453d59b14eee0282439b34e78e6
caijuemou <89962192+caijuemou@users.noreply.github.com>
2026-02-11 10:30:29 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -18,14 +18,14 @@ if (cmbTrackLib && typeof cmbTrackLib.init === 'function') {
       apiInvoke: true,
       defaultErrorCode: '9999'
     });
-
+/*
     const urlParams = new URLSearchParams(window.location.search);
     const data = urlParams.get('data');
     if (data) {
       const theRequestData = encodeURIComponent(data.replace(/''/g, '+'));
       cmbTrackLib.aliasID(theRequestData);
     }
-    
+*/    
     // 挂载到全局
     app.config.globalProperties.cmbTrackLib = cmbTrackLib;
   } catch (err) {
